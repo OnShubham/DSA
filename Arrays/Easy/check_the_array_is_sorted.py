7957f7548cdff7842604de60f933894033cf1106
dishoomshubam <shubham.kharche@insightbridge.co>
2025-10-29 09:56:16 +0530

    add

--- Arrays/Easy/check_the_array_is_sorted.py
+++ Arrays/Easy/check_the_array_is_sorted.py
@@ -14,9 +14,22 @@ def check_arr_sorted(arr, n):
     
 
 
+
+def check_arr_sorted_2(arr,n):
+    
+    for i in range(n):
+        
+        for j in range(i + 1, n):
+            
+            if arr[j] < arr[i]:
+                
+                return print(arr[j],arr[i],"fasle")
+            
+    return print("true")
+
 if __name__ == "__main__":
-    arr = [3,4,5,1,2]
+    arr = [1,2]
     x = arr
     n = len(arr)
     # larget_num(arr, n)
-    check_arr_sorted(arr, n)
+    check_arr_sorted_2(arr, n)
--- Step_2 Important Sorting Techniques/Sorting_2/practice.py
+++ Step_2 Important Sorting Techniques/Sorting_2/practice.py
@@ -56,6 +56,49 @@ def buble_sort(arr,n):
 
 
 
+
+def merger(arr):
+    
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    
+    left = merger(arr[mid:])
+    right = merger(arr[:mid])
+    
+    return merg(left, right)
+
+def merg(left, right):
+    
+    result = []
+    
+    i = j = 0
+    
+    for i in range(n - 1):
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                
+                result.append(left[i])
+                
+                i += 1
+                
+            else:
+                result.append(right[j])
+                
+                i += 1
+                
+    
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+    
+    return result
+    
+    
+
+
 if __name__ == "__main__":
     
     arr = [2,3,2,1,4,5,2,1,2,3,3,4]
@@ -64,3 +107,9 @@ if __name__ == "__main__":
     print("buble", arr)
     a = merger(arr)
     print("test :", a)
+    
+    
+    
+    
+    
+    
